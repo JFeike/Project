3b2a932eca646400d3fddfb3d08080e4c6c0588b
BuildTools <unconfigured@null.spigotmc.org>
2019-11-24 13:16:00 +0100

    Branch "Proto1" erstellt

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,5 +1,12 @@
 
 public class Main {
-
+	
+	public static void main(String[] args) {
+		new Gui();
+		new Variables();
+		new InputHandler();
+		new Graphic();
+		new PlayerMovement();
+		new Collisions();
+	}
 }
-// Test 
